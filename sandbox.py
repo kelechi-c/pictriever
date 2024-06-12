a9e48336fde73d4ab017125a32e8a32d9e0bf5de
Tensor <narutokelechi@gmail.com>
2024-06-12 06:41:19 +0100

    fixed image downloading script

--- sandbox.py
+++ sandbox.py
@@ -1,108 +1,63 @@
-# %%
-import requests, os, shutil, rich, pprint
+import requests, os, shutil, rich, argparse
 
 from tqdm.auto import tqdm
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 
-# %%
 image_folder = "moondream_images"
 # os.mkdir(image_folder)
 out_folder = os.path.join(os.getcwd(), image_folder)
-
 pexel_url = "https://www.pexels.com/"
-pexel_img_class = 'MediaCard_image__yVXRE'
+pexel_img_class = "MediaCard_image__yVXRE"
 
-# %%
+# Webdriver initialization
 drive_opts = Options()
-
-drive_opts.add_argument('-headless')
+drive_opts.add_argument("-headless")
 
 driver = webdriver.Firefox(options=drive_opts)
 
-driver
-
-# %%
-## Test cell
+# get site content
 
 page = driver.get(pexel_url)
 driver.implicitly_wait(100)
 
-driver, page
+# get image tags
+image_tags = driver.find_elements(By.CSS_SELECTOR, f"img.{pexel_img_class}")
 
-# %%
-image_links = driver.find_elements(By.CSS_SELECTOR, f'img.{pexel_img_class}')
+image_urls = [link.get_attribute("src") for link in image_tags] # Then get the links themselves
 
-image_links, len(image_links)
 
-# %%
-img_elements = driver.find_elements(By.TAG_NAME, 'img')
-
-len(img_elements), img_elements
-
-# %%
-img_urls = [link.get_attribute('src') for link in image_links]
-
-len(img_urls), img_urls
-
-# %%
-im_link_list = [os.path.basename(url) for url in img_urls]
-
-rich.print(f"[bold white]{im_link_list}")
-
-# %%
 def format_filename(file_name: str):
-    text = file_name.split('.jpeg')[0]
+    text = file_name.split(".jpeg")[0]
+    text = text + '.jpeg'
+    
     return text
 
-# %%
+
 def download_images(link_list: list):
-    
+
     k = 0
     file_list = []
-    
+
     for image_link in tqdm(link_list):
         try:
             file_res = requests.get(image_link, stream=True)
             image_file = format_filename(os.path.basename(image_link))
-            
-            with open(image_file) as file_writer:
+            file_path = os.path.join(out_folder, image_file)
+
+            with open(file_path, "wb") as file_writer:
                 file_res.raw.decode_content = True
                 shutil.copyfileobj(file_res.raw, file_writer)
-        
-            
+
+            rich.print(f"[bold green] {image_file} successfully downloaded :lightning:")
             file_list.append(image_file)
-            
+            k += 1
+
         except Exception as e:
-            rich.print(f'[bright red]Well...-> {e}')
+            rich.print(f"Well...-> [bold red]{e}[/bold red]")
             continue
-        
-    
-        
-
-# %%
-rich.inspect(download_images)
-
-# %%
-
-r = requests.get(img_urls[1], stream=True)
-if r.status_code == 200:  # 200 status code = OK
-    with open('one.jpg', "wb") as f:
-        r.raw.decode_content = True
-        shutil.copyfileobj(r.raw, f)
-
-# %%
-def get_file_links(site, num_limit):
-    
-    
-    return site
-
-# %%
-class PexelImages():
-    def __init__(self, search_term, count_limit):
-        self.image_type = search_term
-        self.count_limit = count_limit
-        self.pexel_url = f'https://www.pexels.com/search/{search_term}/'
 
 
+if __name__=='main':
+    download_images(image_urls)
