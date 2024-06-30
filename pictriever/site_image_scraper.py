32389cd8c68f0727ba8f9c630e670c0bbd0b0c62
Tensor <narutokelechi@gmail.com>
2024-06-30 03:00:31 +0100

    pexels image scraping class

--- pictriever/site_image_scraper.py
+++ pictriever/site_image_scraper.py
@@ -1,3 +1,6 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.
 import requests
 import os
 import time
@@ -8,38 +11,102 @@ import rich
 import argparse
 from tqdm.auto import tqdm
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.common.by import By
+webdriver.
 
 
-class PexelsImageScraper:
-    def __init__(self, search_term: str):
+class Pexels():
+    def __init__(self, search_term: str, max_count: int, min_count: int):
         self.search_term = self._format_string(search_term)
         self.pexel_url = f"https://www.pexels.com/search/{self.search_term}"
         self.pexel_img_class = "MediaCard_image__yVXRE"
+        self.max_count = max_count
+        self.min_count = min_count
+        self.driver = self._init_webdriver()
+
+    def _format_string(self, search_string: str):  # to remove spces in search terms
+        return search_string.replace(' ', '+')
 
     def _init_webdriver(self):
         drive_opts = Options()
-        drive_opts.add_argument("-headless")  # for no-GUI browsing
+        drive_opts.add_argument('-headless')
         driver = webdriver.Firefox(options=drive_opts)
         driver.maximize_window()
-        driver.get(self.pexel_url)  # load page
+        driver.get(self.pexel_url)
+        print('webdriver initialized')
+        return driver
 
-    def _format_string(search_string: str):  # to remove spces in search terms
-        return search_string.replace(" ", "")
+    def _scrolldelay(self):
+        time.sleep(2)
+        # scrolling down slowly
+        stopScrolling = 0
+        print('scrolling to load more')
+        while True:
+            stopScrolling += 1
+            self.driver.execute_script("window.scrollBy(0,40)")
+            time.sleep(0.1)
+            if stopScrolling > math.floor(self.min_count/2):
+                break
+            time.sleep(1)
 
-    def fetch_image_links(self):
-        self._init_webdriver()
-        image_links = driver.find_elements(
-            By.CLASS_NAME, f"img.{self.pexel_img_class}"
-        )  # get all image links
-        link_count = len(image_links)
-        print(
-            f"total number of acquired links => {link_count}"
-        )  # print number of links
+    def get_imlinks(self, get_all_links: bool = False):
+        if self.min_count > 100:
+            self._scrolldelay(self.min_count)
+        try:
+            driver = self.driver
+            image_links = driver.find_elements(
+                By.CSS_SELECTOR, f"img.{self.pexel_img_class}")
+#             image_links = image_links.find_elements(By.TAG_NAME, 'a')
+
+#             print(image_links)
+            image_links = [link.get_attribute('src') for link in image_links]
+            print(image_links)
+            link_count = len(image_links)
+            links = image_links[:self.max_count] if get_all_links else image_links
+            # print number of links
+            print(
+                f'{link_count} links acquired from Google, using {len(links)} of them')
+
+            return links
+
+        except Exception as e:
+            print(f'e => [{e}]')
+
+    def _format_filename(self, file_name: str):
+        text = file_name.split('.')[0]
+        return text
+
+    def download_images(self, link_list: list, output_folder: str, verbose: bool = False):
+        k = 0
+        os.mkdir(output_folder)
+
+        for image_link in tqdm(link_list):
+            try:
+                # fetch content from url
+                file_res = requests.get(image_link, stream=True)
+                image_file = os.path.basename(image_link)
+                image_file = os.path.join(
+                    output_folder, f'pexel_{self.search_term}_{k}.png')  # output file path
+
+                with open(image_file, "wb") as file_writer:
+                    file_res.raw.decode_content = True
+                    # save to folder
+                    shutil.copyfileobj(file_res.raw, file_writer)
+
+                if verbose:  # show download count and progress
+                    print(f'image @ {k+1} downloaded')
+                k += 1  # file counter
+
+            except Exception as e:  # exception handling
+                print(f"[> {e}]")
+                continue
+
+        print(f'{k} images downloaded from Pexels.com')  # sucess message
+
+    def quit_driver(self):
+        self.driver.quit()
 
 
-class GoogleImageScraper:
+class GoogleImageScraper():
     def __init__(self, search_term: str, max_count: int, min_count: int):
         self.image_class = "YQ4gaf"
         self.search_term = self._format_string(search_term)
@@ -48,7 +115,7 @@ class GoogleImageScraper:
         self.min_count = min_count
         self.driver = self._init_webdriver()
 
-    def _format_string(search_string: str):  # to remove spces in search terms
+    def _format_string(self, search_string: str):  # to remove spces in search terms
         return search_string.replace(" ", "")
 
     def _init_webdriver(self):
