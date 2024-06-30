73df26fba5f42f114296c46d1895ce5ac49a4a55
Tensor <narutokelechi@gmail.com>
2024-06-30 18:47:43 +0100

    fix redundancy in all classes, add driver termination post-execution

--- pictriever/image_scraper.py
+++ pictriever/image_scraper.py
@@ -1,4 +1,8 @@
-import rich, argparse, shutil, sys, os, time, requests, math
+import shutil
+import os
+import time
+import requests
+import math
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 from tqdm.auto import tqdm
@@ -8,6 +12,12 @@ from selenium import webdriver
 # utility functions
 def _format_string(search_string: str):  # to remove spaces in search terms
     return search_string.replace(' ', '+')
+
+
+def _format_filename(file_name: str):
+    text = file_name.split(".")[0]
+    return text
+
 
 def _scrolldelay(driver, min_count):
     time.sleep(2)
@@ -21,6 +31,7 @@ def _scrolldelay(driver, min_count):
         if stopScrolling > math.floor(min_count/2):
             break
         time.sleep(1)
+
 
 def _init_webdriver(url):
     drive_opts = Options()
@@ -39,7 +50,7 @@ class Pexelscraper():
         self.pexel_img_class = "MediaCard_image__yVXRE"
         self.max_count = max_count
         self.min_count = min_count
-        self.driver =  _init_webdriver(self.pexel_url)
+        self.driver = _init_webdriver(self.pexel_url)
 
     def _scrolldelay(self):
         time.sleep(2)
@@ -60,7 +71,8 @@ class Pexelscraper():
 
         try:
             driver = self.driver
-            image_links = driver.find_elements(By.CSS_SELECTOR, f"img.{self.pexel_img_class}")
+            image_links = driver.find_elements(
+                By.CSS_SELECTOR, f"img.{self.pexel_img_class}")
 #             image_links = image_links.find_elements(By.TAG_NAME, 'a')
 
 #             print(image_links)
@@ -70,7 +82,7 @@ class Pexelscraper():
             links = image_links[:self.max_count] if get_all_links else image_links
             # print number of links
             print(
-                f'{link_count} links acquired from Google, using {len(links)} of them')
+                f'{link_count} links acquired from Pexels, using {len(links)} of them')
 
             return links
 
@@ -107,8 +119,10 @@ class Pexelscraper():
                 continue
 
         print(f'{k} images downloaded from Pexels.com')  # sucess message
+        _quit_driver()
 
-    def quit_driver(self):
+    def _quit_driver(self):
+        print('shutting down webdriver...')
         self.driver.quit()
 
 
@@ -119,36 +133,11 @@ class GoogleImageScraper():
         self.search_url = f"https://www.google.com/search?q={self.search_term}l&sca_esv=fb64c27acdf6db0d&sca_upv=1&udm=2&sxsrf=ADLYWII43-tIV80076FgCtRKI2_M4AdexQ%3A1719652914088&ei=MtJ_ZvCOBar2kdUP_sOt0AM&ved=0ahUKEwjw79KovoCHAxUqe6QEHf5hCzoQ4dUDCBA&uact=5&oq=diffusionmodel&gs_lp=Egxnd3Mtd2l6LXNlcnAiDmRpZmZ1c2lvbm1vZGVsMgYQABgHGB4yBhAAGAcYHjIGEAAYBxgeMgYQABgHGB4yBhAAGAcYHjIGEAAYBxgeMgYQABgHGB4yBhAAGAcYHjIGEAAYBxgeMgYQABgHGB5IoBNQmwVYmwVwAXgAkAEAmAH0BaAB9AWqAQM2LTG4AQPIAQD4AQGYAgKgAqwGwgIKEAAYgAQYQxiKBcICBRAAGIAEmAMAiAYBkgcFMS41LTGgB9oF&sclient=gws-wiz-serp"
         self.max_count = max_count
         self.min_count = min_count
-        self.driver = self._init_webdriver()
-
-    def _format_string(self, search_string: str):  # to remove spces in search terms
-        return search_string.replace(" ", "")
-
-    def _init_webdriver(self):
-        drive_opts = Options()
-        drive_opts.add_argument("-headless")
-        driver = webdriver.Firefox(options=drive_opts)
-        driver.maximize_window()
-        driver.get(self.search_url)
-        print("webdriver initialized")
-        return driver
-
-    def _scrolldelay(self):
-        time.sleep(2)
-        # scrolling down slowly
-        stopScrolling = 0
-        print("scrolling to load more")
-        while True:
-            stopScrolling += 1
-            self.driver.execute_script("window.scrollBy(0,40)")
-            time.sleep(0.1)
-            if stopScrolling > math.floor(self.min_count / 2):
-                break
-            time.sleep(1)
+        self.driver = _init_webdriver()
 
     def get_imlinks(self, get_all_links: bool = False):
         if self.min_count > 100:
-            self._scrolldelay(self.min_count)
+            _scrolldelay(self.driver, self.min_count)
         try:
             image_links = self.driver.find_elements(
                 By.CLASS_NAME, self.image_class)
@@ -166,10 +155,6 @@ class GoogleImageScraper():
             return links
         except Exception as e:
             print(f"e => [{e}]")
-
-    def _format_filename(file_name: str):
-        text = file_name.split(".")[0]
-        return text
 
     def download_images(
         self, link_list: list, output_folder: str, verbose: bool = False
@@ -199,8 +184,10 @@ class GoogleImageScraper():
                 continue
 
         print(f"{k} images downloaded from Google")  # sucess message
+        _quit_driver()
 
-    def quit_driver(self):
+    def _quit_driver(self):
+        print('shutting down webdriver...')
         self.driver.quit()
 
 
@@ -218,13 +205,15 @@ class BingImages():
             _scrolldelay(self.driver, self.min_count)
 
         try:
-            image_links = self.driver.find_elements(By.CSS_SELECTOR, f"a.{self.bing_imlink_class}")
+            image_links = self.driver.find_elements(
+                By.CSS_SELECTOR, f"a.{self.bing_imlink_class}")
             image_links = [link.get_attribute('href') for link in image_links]
             print(image_links)
             link_count = len(image_links)
             links = image_links[:self.max_count] if get_all_links else image_links
             # print number of links
-            print(f'{link_count} links acquired from Bing images, using {len(links)}...')
+            print(
+                f'{link_count} links acquired from Bing images, using {len(links)}...')
             return links
 
         except Exception as e:
@@ -257,11 +246,12 @@ class BingImages():
                 continue
 
         print(f'{k} images downloaded from Bing images')  # sucess message
+        _quit_driver()
+
 
     def _quit_driver(self):
+        print('shutting down webdriver...')
         self.driver.quit()
-
-
 
 
 class UnsplashImages():
@@ -278,7 +268,7 @@ class UnsplashImages():
             _scrolldelay(self.driver, self.min_count)
 
         try:
-            image_links = self.driver.find_elements(By.CSS_SELECTOR, f"a.{self.unsplash_class}")
+            image_links = self.driver.find_elements(By.CSS_SELECTOR, f"img.{self.unsplash_class}")
             image_links = [link.get_attribute('src') for link in image_links]
             print(image_links)
             link_count = len(image_links)
@@ -317,7 +307,9 @@ class UnsplashImages():
                 continue
 
         print(f'{k} images downloaded from unsplash.com images')  # sucess message
+        _quit_driver()
 
     def _quit_driver(self):
+        print('shutting down webdriver...')
         self.driver.quit()
 
