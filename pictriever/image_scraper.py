05261ca8c4ac255062afa43f0adebd8d9f7660c3
Tensor <narutokelechi@gmail.com>
2024-06-30 17:50:04 +0100

    add class for Unsplash scraping

--- pictriever/image_scraper.py
+++ pictriever/image_scraper.py
@@ -181,8 +181,7 @@ class GoogleImageScraper():
                 file_res = requests.get(
                     image_link, stream=True
                 )  # fetch content from url
-                image_file = self._format_filename(
-                    os.path.basename(image_link))
+                image_file = _format_filename(os.path.basename(image_link))
                 image_file = os.path.join(
                     output_folder, image_file)  # output file path
 
@@ -258,6 +257,66 @@ class BingImages():
                 continue
 
         print(f'{k} images downloaded from Bing images')  # sucess message
+
+    def _quit_driver(self):
+        self.driver.quit()
+
+
+
+
+class UnsplashImages():
+     def __init__(self, search_term: str, min_count: int, max_count: int):
+        self.search_term = _format_string(search_term)
+        self.unsplash_url = f'https://unsplash.com/s/photos/{self.search_term}'
+        self.unsplash_class = 'vkrMA'
+        self.max_count = max_count
+        self.min_count = min_count
+        self.driver = _init_webdriver(self.unsplash_url)
+
+     def get_imlinks(self, get_all_links: bool = False):
+        if self.min_count > 100:
+            _scrolldelay(self.driver, self.min_count)
+
+        try:
+            image_links = self.driver.find_elements(By.CSS_SELECTOR, f"a.{self.unsplash_class}")
+            image_links = [link.get_attribute('src') for link in image_links]
+            print(image_links)
+            link_count = len(image_links)
+            links = image_links[:self.max_count] if get_all_links else image_links
+            # print number of links
+            print(f'{link_count} links acquired from Unsplash, using {len(links)}...')
+            return links
+
+        except Exception as e:
+            print(f'e => [{e}]')
+
+
+    def download_images(self, link_list: list, output_folder: str, verbose: bool = False):
+        k = 0
+        if not os.path.exists(output_folder):
+            os.mkdir(output_folder)
+
+        for image_link in tqdm(link_list):
+            try:
+                # fetch content from url
+                file_res = requests.get(image_link, stream=True)
+                image_file = os.path.basename(image_link)
+                image_file = os.path.join(output_folder, f'unsplash_{self.search_term}_{k}.png')  # output file path
+
+                with open(image_file, "wb") as file_writer:
+                    file_res.raw.decode_content = True
+                    # save to folder
+                    shutil.copyfileobj(file_res.raw, file_writer)
+
+                if verbose:  # show download count and progress
+                    print(f'image @ {k+1} downloaded')
+                k += 1  # file counter
+
+            except Exception as e:  # exception handling
+                print(f"[> {e}]")
+                continue
+
+        print(f'{k} images downloaded from unsplash.com images')  # sucess message
 
     def _quit_driver(self):
         self.driver.quit()
